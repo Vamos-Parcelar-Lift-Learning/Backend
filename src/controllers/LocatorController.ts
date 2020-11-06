269b7d0358df9c944a51668a73646c37bdc66ffc
matheus456 <matheus456456@gmail.com>
2020-11-06 18:14:16 -0300

    Altera validação para controller

--- src/controllers/LocatorController.ts
+++ src/controllers/LocatorController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 import LocatorService from '../services/LocatorService';
+import AppError from '../errors/AppError';
 
 export default class LocatorController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -12,8 +14,16 @@ export default class LocatorController {
     request: Request,
     response: Response,
   ): Promise<Response> {
-    const locatorService = new LocatorService();
     const { code } = request.params;
+    const schema = yup
+      .string()
+      .length(6)
+      .matches(/[A-Z0-9]{6}/);
+    const isValid = await schema.isValid(code);
+    if (!isValid) {
+      throw new AppError('Localizador não encontrado.', 404);
+    }
+    const locatorService = new LocatorService();
     const locators = await locatorService.getLocator(code);
     return response.status(200).json({ locators });
   }
--- src/services/LocatorService.ts
+++ src/services/LocatorService.ts
@@ -1,35 +1,24 @@
 import { getMongoRepository } from 'typeorm';
-import * as yup from 'yup';
 import Locator from '../schemas/Locator';
 import AppError from '../errors/AppError';
 
 class LocatorService {
   public async getAllLocators(): Promise<Locator[]> {
-    const schema = yup.array().min(1);
     const locatorRepository = getMongoRepository(Locator, 'mongo');
     const locators = await locatorRepository.find();
-    const isValid = await schema.isValid(locators);
-    if (isValid) {
-      return locators;
+    if (!locators.length) {
+      throw new AppError('Nenhum localizador encontrado', 404);
     }
-    throw new AppError('Nenhum localizador encontrado', 404);
+    return locators;
   }
 
   public async getLocator(code: string): Promise<Locator | undefined> {
-    const schema = yup
-      .string()
-      .length(6)
-      .matches(/[A-Z0-9]{6}/);
-
-    const isValid = await schema.isValid(code);
-    if (isValid) {
-      const locatorRepository = getMongoRepository(Locator, 'mongo');
-      const locator = await locatorRepository.findOne({ code });
-      if (locator) {
-        return locator;
-      }
+    const locatorRepository = getMongoRepository(Locator, 'mongo');
+    const locator = await locatorRepository.findOne({ code });
+    if (!locator) {
+      throw new AppError('Localizador não encontrado.', 404);
     }
-    throw new AppError('Localizador não encontrado.', 404);
+    return locator;
   }
 }
 
