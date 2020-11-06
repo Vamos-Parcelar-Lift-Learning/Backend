dcbafca8222ef3961e02e089f0cd851537bb5cca
matheus456 <matheus456456@gmail.com>
2020-11-05 21:37:34 -0300

    Adiciona validação do campo code

--- src/services/LocatorService.ts
+++ src/services/LocatorService.ts
@@ -1,4 +1,5 @@
 import { getMongoRepository } from 'typeorm';
+import * as yup from 'yup';
 import Locator from '../schemas/Locator';
 
 class LocatorService {
@@ -9,9 +10,18 @@ class LocatorService {
   }
 
   public async getLocator(code: string): Promise<Locator | undefined> {
-    const locatorRepository = getMongoRepository(Locator, 'mongo');
-    const locator = await locatorRepository.findOne({ code });
-    return locator;
+    const schema = yup
+      .string()
+      .length(6)
+      .matches(/[A-Z0-9]{6}/);
+
+    const isValid = await schema.isValid(code);
+    if (isValid) {
+      const locatorRepository = getMongoRepository(Locator, 'mongo');
+      const locator = await locatorRepository.findOne({ code });
+      return locator;
+    }
+    return undefined; // Tratar erro
   }
 }
 
